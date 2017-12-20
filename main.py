577fdb1479a68c0bb0eb4c5b9b5ccc3023ea8c58
Steve0x2a <stevesough@gmail.com>
2017-12-20 23:13:06 +0800

    重构

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
-from bs4 import BeautifulSoup
-from parse import get_webflow
+from parse import get_webflow,get_stuinfo
+import getpass
 
 class login_gzdx(object):
     def __init__(self,headers):
@@ -8,11 +8,18 @@ class login_gzdx(object):
         self.headers = headers
         self.baseUrl = 'http://202.192.18.184'
         self.url = 'https://cas.gzhu.edu.cn/cas_server/login?service=http%3a%2f%2f202.192.18.184%2fLogin_gzdx.aspx'
-    
+        self.stuinfo = {}
+
     def login(self, account, passwd):
+        '''
+
+        :param account: 传入学号
+        :param passwd:传入密码
+        :模拟登陆主要部分
+        '''
         self.username = account
         self.password = passwd
-        get_lt = self.session.get(url = self.url,headers = self.headers)
+        get_lt = self.session.get(url = self.url, headers = self.headers)
         lt, execution = get_webflow(get_lt)
         postdata = {
             'username' : account,
@@ -28,9 +35,18 @@ class login_gzdx(object):
             print('成功')
             infourl = self.baseUrl+"/xsgrxx.aspx?xh="+account+"&"
             info = self.session.get(infourl,headers = headers)
-            self.get_stuinfo(info)
-            #text = self.session.get('http://my.gzhu.edu.cn/jw',headers = headers)
-            '''text = self.session.get(jwurl,headers = headers)
-            print(text.text)
-        else:
-            print(response.text)'''
+            self.stuinfo = get_stuinfo(info)
+            print("欢迎你, {}, 您的学号为:{}".format(self.stuinfo["name"],self.stuinfo["studentnumber"]))
+
+if __name__ == '__main__':
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36",
+    }
+    login = login_gzdx(headers = headers)
+    account = input('输出学号')
+    password = getpass.getpass("请输入您的密码:")
+    login.login(account = account,passwd =password)
+
+
+
+
--- parse.py
+++ parse.py
@@ -1,6 +1,29 @@
-    def get_webflow(response):
-        soup = BeautifulSoup(response.text,'html.parser')
-        lt = soup.find('input',{'name' : 'lt'})['value']
-        execution = soup.find('input',{'name' : 'execution'})['value']
-        soup.clear()
-        return(lt,execution)
+from bs4 import BeautifulSoup
+
+def get_webflow(response):
+    soup = BeautifulSoup(response.text,'html.parser')
+    lt = soup.find('input',{'name' : 'lt'})['value']
+    execution = soup.find('input',{'name' : 'execution'})['value']
+    soup.clear()
+    return(lt,execution)
+
+def get_stuinfo(response):
+    html = response.text
+    soup = BeautifulSoup(html, 'html.parser')
+    d = {}
+    d["studentnumber"] = soup.find(id="xh").string
+    d["idCardNumber"] = soup.find(id="lbl_sfzh").string
+    d["name"] = soup.find(id="xm").string
+    d["sex"] = soup.find(id="lbl_xb").string
+    d["enterSchoolTime"] = soup.find(id="lbl_rxrq").string
+    d["birthsday"] = soup.find(id="lbl_csrq").string
+    d["highschool"] = soup.find(id="lbl_byzx").string
+    d["nationality"] = soup.find(id="lbl_mz").string
+    d["hometown"] = soup.find(id="lbl_jg").string
+    d["politicsStatus"] = soup.find(id="lbl_zzmm").string
+    d["college"] = soup.find(id="lbl_xy").string
+    d["major"] = soup.find(id="lbl_zymc").string
+    d["classname"] = soup.find(id="lbl_xzb").string
+    d["gradeClass"] = soup.find(id="lbl_dqszj").string
+    return d
+
